bf47233413f70509031a6210139f764ace7d0cfc
palinkiewicz <pogromca.ap@gmail.com>
2024-09-15 16:03:17 +0200

    Add the code for the second lab's first task

---
+++ lab_2/Zadanie1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(nullptr));
+
+    double temps[50][40][27];
+
+    uint8_t maxIndex = 0;
+    double maxAvg = 0.0;
+
+    for (uint8_t i = 0; i < 50; i++) {
+        double avg = 0.0;
+
+        for (uint8_t j = 0; j < 40; j++) {
+            for (uint8_t k = 0; k < 27; k++) {
+                temps[i][j][k] = (double)(rand() % 701 + 1800) / 100.0;
+                avg += temps[i][j][k];
+            }
+        }
+
+        avg /= 40 * 27;
+
+        if (avg > maxAvg) {
+            maxIndex = i;
+            maxAvg = avg;
+        }
+    }
+
+    SetConsoleOutputCP(CP_UTF8);
+    std::cout << u8"Największa średnia temperatura (" << maxAvg << "^C) odnaleziona w przekroju " << (short) maxIndex << ".\n";
+}
