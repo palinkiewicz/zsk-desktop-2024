83e4d59abe71bcb1152f7203686dcc5e838f4db4
palinkiewicz <pogromca.ap@gmail.com>
2024-09-18 14:59:42 +0200

    Add some code that explains RAM

---
+++ lab_3/Cwiczenie1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+    int x;
+    // std::cout << x << "\n";
+
+    x = 10;
+    std::cout << x << "\n";
+
+    int* adres = &x;
+    std::cout << adres << "\n";
+
+    int* y;
+    // std::cout << y << "\n";
+
+    y = new int;
+    std::cout << y << "\n";
+
+    *y = 20;
+    std::cout << *y << "\n";
+
+    delete y;
+    std::cout << y << "\n";
+}
